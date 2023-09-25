6865664018edce41cba5d7ec95d7dcce9612fc15
harrydehix <harry.holly02@gmail.com>
2023-09-25 16:38:21 +0200

    Handle exit

--- src/tests/api.ts
+++ src/tests/api.ts
@@ -5,3 +5,20 @@ api.configure({
     });
 
 api.start();
+
+function handleExit(){
+    api.stop();
+}
+
+// do something when app is closing
+process.on('exit', handleExit);
+
+// catches ctrl+c event
+process.on('SIGINT', handleExit);
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', handleExit);
+process.on('SIGUSR2', handleExit);
+
+// catches uncaught exceptions
+process.on('uncaughtException', handleExit);
